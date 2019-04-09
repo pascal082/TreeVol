8d28055d8493373d09c66ccec836b131717fa494
LANDCARE\omondiagbep <omondiagbep@landcareresearch.co.nz>
2019-04-09 16:51:43 +1200

    adding test cases

--- tests/testthat/test-vol-trees.R
+++ tests/testthat/test-vol-trees.R
@@ -1,9 +1,6 @@
-
-test_that("Test if input value pass to tree volumn is a dataframe ", {
-  x=2
-  #vol.Trees<-volTrees(x)
-  
-  is.data.frame()
-  
-  expect_equal(is.data.frame(x),TRUE,info="The input value passed to the Tree vloumn must be a dataframe")
-})
+context("VolTree")
+test_that("Tree volumn should not work if the input is not a dataframe ", {
+   #pass non dataframe 
+   x=2
+  expect_error(volTrees(x),"The input value is not a dataframe",info="The input value passed to the Tree vloumn must be a dataframe")
+ })
